37e167ea2db23beed63ac11c2665ed37eaaeb5d3
mattkantor <matthewkantor@gmail.com>
2015-11-10 05:40:39 -0500

    reorg folders

--- lib/alchemy-news-api.rb
+++
@@ -1,7 +0,0 @@
-require "alchemy-news-api/connection.rb"
-require "alchemy-news-api/client.rb"
-require "alchemy-news-api/news_item.rb"
-require "alchemy-news-api/concept.rb"
-require "alchemy-news-api/version.rb"
-
-
---
+++ lib/alchemy-news-sdk.rb
@@ -0,0 +1,7 @@
+require "alchemy-news-sdk/connection.rb"
+require "alchemy-news-sdk/client.rb"
+require "alchemy-news-sdk/news_item.rb"
+require "alchemy-news-sdk/concept.rb"
+require "alchemy-news-sdk/version.rb"
+
+
